27cc75a0678accd245e4b0802ecffd6040832768
Tyler <saylestyler@gmail.com>
2017-07-14 18:22:11 -0400

    wip stylin

--- ixporter.py
+++ ixporter.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# from https://gist.github.com/nslater/b3cbc894ad2c2516dd02
-
 import sys
 import urllib
 import urlparse
@@ -15,7 +13,7 @@ import sqlite3
 
 CHAT_DB = path.expanduser("~/Library/Messages/chat.db")
 
-EPOCH=978307200
+EPOCH = 978307200
 
 print("""
     <!doctype html>
@@ -24,7 +22,7 @@ print("""
     <meta charset=\"utf-8\">
     <style>
     body { 
-        width: 100%; 
+        width: 100%;
         margin: 0px;
     }
     .message {
@@ -32,44 +30,28 @@ print("""
         padding: 25px;
         margin: 30px auto;
         border-radius: 3px;
-        font-size: 24px;
+        font-size: 22px;
+        font-family: 'Helvetica';
     }
-    .me { 
-        background-color: #c2ffe4; 
-    }
-    .buddy { 
-        background-color: #e4c2ff; 
-    }
+    .me { background-color: #c2ffe4; }
+    .friend { background-color: #faf; }
     .message img {
         max-width: 760px;
         margin: 20px;
     }
-    hr {
-        width: 75%;
-    }
+    hr { width: 75%; }
     </style>
     </head>
     <body>
 """)
-
-def list_chats():
-    db = sqlite3.connect(CHAT_DB)
-    cursor = db.cursor()
-    rows = cursor.execute("""
-        SELECT chat_identifier
-          FROM chat
-          ORDER by desc
-          LIMIT 2;
-    """)
-    for row in rows:
-        print(row[0])
 
 def export_all():
     db = sqlite3.connect(CHAT_DB)
     cursor = db.cursor()
     rows = cursor.execute("""
         SELECT chat_identifier
-          FROM chat;
+          FROM chat
+          LIMIT 6;
     """)
     for row in rows:
         export(row[0])
@@ -96,10 +78,9 @@ def export(chat_id):
         ORDER BY m.date;
     """, (EPOCH, chat_id))
 
-
     for row in rows:
         date = row[0]
-        who = "me" if row[1] is 1 else "buddy"
+        who = "me" if row[1] is 1 else "friend"
         if row[3]:
             attachment = path.expanduser(row[3])
             media_type = mimetypes.guess_type(attachment)[0]
@@ -108,30 +89,11 @@ def export(chat_id):
                     encoded_data = base64.b64encode(image.read())
             except:
                 encoded_data = ""
-            # text = "<img src=\"data:%s;base64,%s\">" % (media_type, encoded_data) # to encode images as strings/host
-            text = "<img src=\"file://%s\">" % (attachment) # to serve images locally
+            text = "<img src=\"data:%s;base64,%s\">" % (media_type, encoded_data)
         else:
             text = cgi.escape(row[2] or '')
-        line = "<div class=\"message %s\" title=\"%s\">%s</div> " % (
-                who, date, text)
+        line = "<div class=\"message %s\" title=\"%s\">%s</div> " % (who, date, text)
         print(line.encode("utf8"))
-        # print(who, date) # here = 'me' || buddy + utc
-        print("""
-            </body>
-            </html>
-        """)
+        print("</body></html>")
 
-def main():
-    if len(sys.argv) == 1:
-        export_all()
-    sys.exit()
-chat_id = None
-if len(sys.argv) > 1:
-    chat_id = sys.argv[1]
-if len(sys.argv) > 2:
-    sys.exit()
 export_all()
-
-if __name__ == "__main__":
-    main()
-
