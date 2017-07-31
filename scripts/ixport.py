49a5b87ce9f28f9fc56cf697dd2e30c01ad1f891
Tyler <saylestyler@gmail.com>
2017-07-31 19:49:44 -0400

    prints formatted phonenumbers from address book db

--- scripts/ixport.py
+++ scripts/ixport.py
@@ -8,19 +8,35 @@ import datetime
 from os import path
 
 import sqlite3
+import phonenumbers
 
 CHAT_DB = path.expanduser("~/Library/Messages/chat.db")
+ADDRESS_BOOK_DB = path.expanduser('~/Library/Application Support/AddressBook/AddressBook-v22.abcddb')
 
 EPOCH = 978307200
 
 print("<!doctype html><html><head><meta charset=\"utf-8\"><style>body{width:100%;margin:0px;}.message{max-width:600px;padding:25px;margin:30px auto;border-radius: 3px;font-size:22px; font-family: 'Helvetica'; } .me { background-color: #c2ffe4; } .friend { background-color: #faf; } .message img { max-width: 550px; margin: 20px; } hr { width: 75%; } </style> </head> <body>")
+
+def export_all_contacts():
+    db = sqlite3.connect(ADDRESS_BOOK_DB)
+    cursor = db.cursor()
+    rows = cursor.execute("""
+        SELECT ZSTRINGFORINDEXING
+          FROM ZABCDCONTACTINDEX;
+    """)
+    for row in rows:
+        line = row[0]
+        print(line.encode("utf8"))
+        for match in phonenumbers.PhoneNumberMatcher(line, "US"):
+            print phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164)
 
 def export_all():
     db = sqlite3.connect(CHAT_DB)
     cursor = db.cursor()
     rows = cursor.execute("""
         SELECT chat_identifier
-          FROM chat;
+          FROM chat
+         LIMIT 4;
     """)
     for row in rows:
         export(row[0])
@@ -46,12 +62,10 @@ def export(chat_id):
            WHERE c.chat_identifier = ?
         ORDER BY m.date;
     """, (EPOCH, chat_id))
-
     for row in rows:
         unicode_date = row[0]
         formatted_date = datetime.datetime.strptime(unicode_date, '%Y-%m-%d %H:%M:%S').strftime('%m-%d %H:%M')
         who = "me" if row[1] is 1 else "friend"
-
         if row[3]:
             attachment = path.expanduser(row[3])
             media_type = mimetypes.guess_type(attachment)[0]
@@ -60,15 +74,14 @@ def export(chat_id):
                     encoded_data = base64.b64encode(image.read())
             except:
                 encoded_data = ""
-            text = "<img src=\"data:%s;base64,%s\">" % (media_type, encoded_data)
-            # text = "<img src=\"file://%s\">" % (attachment)
+            # text = "<img src=\"data:%s;base64,%s\">" % (media_type, encoded_data)
+            text = "<img src=\"file://%s\">" % (attachment)
         else:
             text = cgi.escape(row[2] or '')
-
         line = "<div class=\"message %s\"> %s <br/> %s</div> " % (who, formatted_date, text)
-
         print(line.encode("utf8"))
-
     print("</body></html>")
 
 export_all()
+export_all_contacts()
+
