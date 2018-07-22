9c4a64c95fd8e062bbe37b46e6494695054aee86
Tyler Sayles <saylestyler@gmail.com>
2018-07-22 14:31:18 -0400

    delete crud & edit css

--- ixport.py
+++ ixport.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import sys
-import urllib
-import urlparse
+# import sys
+# import urllib
+# import urlparse
 import base64
 import mimetypes
 import cgi
@@ -13,7 +13,7 @@ import sqlite3
 
 CHAT_DB = path.expanduser("~/Library/Messages/chat.db")
 
-EPOCH=978307200
+EPOCH = 978307200
 
 print("""
 <!doctype html>
@@ -21,20 +21,17 @@ print("""
 <head>
 <meta charset=\"utf-8\">
 <style>
-body { margin: 0; padding: 0; }
-.message {
-    white-space: pre-wrap;
-    max-width: 800px;
-    padding: 10px;
-    margin: 10px;
-}
-.me { background-color: #A6DBFF; }
-.buddy { background-color: #EEE; }
-.message img { max-width: 800px; }
+body{width:100%;margin:0px;}
+.message{max-width:600px;padding:25px;margin:30px a
+uto;border-radius: 3px;font-size:22px; font-family: 'Helvetica'; }
+.from-me { background-color: #c2ffe4; }
+.friend { background-color: #faf; } .from-contact img { max-w
+idth: 550px; margin: 20px; } hr { width: 75%; }
 </style>
 </head>
 <body>
 """)
+
 
 def list_chats():
     db = sqlite3.connect(CHAT_DB)
@@ -46,6 +43,7 @@ def list_chats():
     for row in rows:
         print(row[0])
 
+
 def export_all():
     db = sqlite3.connect(CHAT_DB)
     cursor = db.cursor()
@@ -56,6 +54,7 @@ def export_all():
     for row in rows:
         export(row[0])
         print('<hr>')
+
 
 def export(chat_id):
     db = sqlite3.connect(CHAT_DB)
@@ -75,7 +74,8 @@ def export(chat_id):
        LEFT JOIN attachment as a
               ON a.ROWID = ma.attachment_id
            WHERE c.chat_identifier = ?
-        ORDER BY m.date;
+        ORDER BY m.date
+           LIMIT 1;
     """, (EPOCH, chat_id))
 
     for row in rows:
@@ -95,7 +95,7 @@ def export(chat_id):
 
         else:
             text = cgi.escape(row[2] or '')
-        line = "<div class=\"from-%s\" title=\"%s\">%s</div>" % (
+        line = "<div class=\"message from-%s\" title=\"%s\">%s</div>" % (
             who, date, text)
         print(line.encode("utf8"))
 
@@ -103,7 +103,6 @@ def export(chat_id):
     </body>
     </html>
     """)
+
+
 export_all()
-
-#list_chats()
-
